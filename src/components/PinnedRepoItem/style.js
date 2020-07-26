951453e16cd69598955a9978520e190e19967e4a
Samuel Reichert <samuelreichertt@gmail.com>
2020-07-26 12:39:21 -0300

    Create PinnedRepoItem component

---
+++ src/components/PinnedRepoItem/style.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const Card = styled.a`
+  border: 1px solid #EBEBEB;
+  border-radius: 4px;
+  color: #102231;
+  cursor: pointer;
+  height: 176px;
+  padding: 20px;
+  text-decoration: none;
+  width: 100%;
+
+  &:hover {
+    background-color: #F8F8F8;
+  }
+`;
+
+export const RepoDescription = styled.span`
+  color: #2F445C;
+  font-size: 14px;
+`;
+
+export const RepoName = styled.p`
+  font-size: 16px;
+  margin: 0 0 12px;
+  padding: 0;
+`;
