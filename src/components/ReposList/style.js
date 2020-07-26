3118bb4b366af3412072311d84ecad130de90e49
Samuel Reichert <samuelreichertt@gmail.com>
2020-07-26 15:08:55 -0300

    Create ReposList component

---
+++ src/components/ReposList/style.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0;
+`;
+
+export const ReposListWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0;
+  padding: 0;
+`
+
+export const Title = styled.h2`
+  font-size: 18px;
+  font-weight: 400;
+  margin: 0 24px 24px;
+  padding: 0;
+`;
