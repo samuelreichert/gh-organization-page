c01f429a5efc3d820366f59aa6f7e9aaa1ed8971
Samuel Reichert <samuelreichertt@gmail.com>
2020-07-26 12:40:26 -0300

    Add PinnedRepos component to App component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { ApolloProvider } from '@apollo/client';
 import { client } from './apollo-client';
 import OrganizationInfo from './components/OrganizationInfo';
+import PinnedRepos from './components/PinnedRepos';
 import { AppContainer } from './App.style';
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <ApolloProvider client={client}>
       <AppContainer>
         <OrganizationInfo />
+        <PinnedRepos />
       </AppContainer>
     </ApolloProvider>
   );
--- src/App.style.js
+++ src/App.style.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const AppContainer = styled.div`
   background-color: #ffffff;
-  border: 1px solid #e1e4e8;
+  border: 1px solid #EBEBEB;
   border-radius: 4px;
   box-shadow: 0 0 3px rgba(0,0,0,.075);
   margin: 40px 0;
