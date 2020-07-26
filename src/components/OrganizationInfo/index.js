756c8bd72de89178d397638bb08026448d7b7258
Samuel Reichert <samuelreichertt@gmail.com>
2020-07-26 11:44:16 -0300

    Create OrganizationInfo component

---
+++ src/components/OrganizationInfo/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { BASIC_ORGANIZATION_INFO } from './query';
+import { BasicInfo, BasicInfoItem, IconWrapper, Image, InfoContainer, Link, Title } from './style';
+import { LinkIcon, LocationIcon } from '../icons';
+
+const OrganizationInfo = () => {
+  const orgLogin = process.env.REACT_APP_GITHUB_ORGANIZATION;
+  const { loading, error, data } = useQuery(BASIC_ORGANIZATION_INFO, { variables: { login: orgLogin }});
+
+  if (loading) return null;
+  if (error) return null;
+
+  const {
+    organization: {
+      avatarUrl,
+      location,
+      name,
+      websiteUrl
+    }
+  } = data;
+
+  return (
+    <InfoContainer>
+      <Image src={avatarUrl} alt={name} />
+      <div>
+        <Title>{name}</Title>
+        <BasicInfo>
+          <BasicInfoItem>
+            <IconWrapper left='3px'>
+              <LocationIcon />
+            </IconWrapper>
+            {location}
+          </BasicInfoItem>
+
+          <BasicInfoItem>
+            <IconWrapper>
+              <LinkIcon />
+            </IconWrapper>
+            <Link>{websiteUrl}</Link>
+          </BasicInfoItem>
+        </BasicInfo>
+      </div>
+    </InfoContainer>
+  );
+};
+
+export default OrganizationInfo;
---
+++ src/components/OrganizationInfo/style.js
@@ -0,0 +1,49 @@
+import styled, { css } from 'styled-components';
+
+export const BasicInfo = styled.ul`
+  color: #6A788B;
+  font-size: 12px;
+  letter-spacing: 0.4px;
+  list-style-type: none;
+  padding: 0;
+`;
+
+export const BasicInfoItem = styled.li`
+  align-items: center;
+  display: flex;
+  margin-bottom: 6px;
+`;
+
+export const IconWrapper = styled.span`
+  ${props => props.left && css`
+    padding-left: ${props.left};
+  `}
+  width: 24px;
+
+  svg {
+    margin-right: 8px;
+  }
+`;
+
+export const Image = styled.img`
+  height: 132px;
+  margin-right: 8px;
+  width: 132px;
+`;
+
+export const InfoContainer = styled.div`
+  align-items: center;
+  display: flex;
+`;
+
+export const Link = styled.a`
+  color: #63A9F3;
+`;
+
+
+export const Title = styled.h1`
+  color: #102231;
+  font-size: 22px;
+  font-weight: 400;
+`;
+
