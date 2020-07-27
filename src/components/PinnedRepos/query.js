93179d9d14b7871f7594d26a197e0eed7c11c7d0
Samuel Reichert <samuelreichertt@gmail.com>
2020-07-26 21:54:19 -0300

    Add new fields to Repos List query and add id in all queries

--- src/components/PinnedRepos/query.js
+++ src/components/PinnedRepos/query.js
@@ -3,6 +3,7 @@ import { gql } from '@apollo/client';
 export const PINNED_REPOSITORIES_FROM_ORG = gql`
   query ($login: String!) {
     organization(login: $login) {
+      id
       pinnedItems(first: 6) {
         nodes {
           ... on Repository {
